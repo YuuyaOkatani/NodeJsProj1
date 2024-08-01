a870a06ddb62d616c0cf97ee088908ffce781d4e
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-08-01 14:11:46 -0300

    my first node project'

---
+++ app.js
@@ -0,0 +1,6 @@
+var calculadora = require("./calculadora"); 
+
+console.log(calculadora.soma(3.14 , 2))
+console.log(calculadora.mult(15,21312 ));
+console.log(calculadora.sub(10, 5));
+console.log(calculadora.div(20, 2));
